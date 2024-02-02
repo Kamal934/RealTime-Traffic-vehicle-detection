3e2fc3d740fad802825a647ebf5bfd4e326ac1d3
Kamal934 <deepkamal7717652661@gmail.com>
2024-02-02 23:46:53 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# from ultralytics import YOLO
+# import cv2
+
+# model= YOLO("best.pt")
+# cap = cv2.VideoCapture("rtsp://50.252.187.218:554/axis-media/media.amp")
+
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     model.predict(frame,  save=True, show=True,conf=0.5)
+#     cv2.imshow('Live Video', frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+    
+# cap.release()
+# cv2.destroyAllWindows()
+
+
+from flask import Flask, render_template, Response
+from ultralytics import YOLO
+import cv2
+
+app = Flask(__name__)
+model = YOLO("runs/detect/train/weights/best.pt")
+cap = cv2.VideoCapture("rtsp://50.252.187.218:554/axis-media/media.amp")
+
+def generate_frames():
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Perform YOLOv5 prediction
+        model.predict(frame, save=True, show=False, conf=0.5)
+        # frame_with_predictions = results.xyxy[0].render()[0]
+
+        # Convert frame to JPEG
+        # ret, buffer = cv2.imencode('.jpg', frame_with_predictions)
+        with open('/home/kamal/python/RealTime-Traffic-vehicle-detection/runs/detect/predict/image0.jpg', 'rb') as file:
+            image_bytes = file.read()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + image_bytes + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
